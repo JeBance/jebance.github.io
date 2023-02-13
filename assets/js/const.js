a4b6a3ba9a19fecb3e5cb936ac163fe92ac719f7
JeBance <oleg.prudkov@gmail.com>
2023-02-13 12:16:06 +0300

    добавил функцию хэширования HMAC

--- assets/js/const.js
+++ assets/js/const.js
@@ -8,6 +8,24 @@ containerElements.hide = function() {
 	for (let i = 0, l = containerElements.length; i < l; i++) containerElements[i].hide();
 }
 
+async function HMAC(key, message)
+{
+	const g = str => new Uint8Array([...unescape(encodeURIComponent(str))].map(c => c.charCodeAt(0))),
+	k = g(key),
+	m = g(message),
+	c = await crypto.subtle.importKey('raw', k, { name: 'HMAC', hash: 'SHA-256' },true, ['sign']),
+	s = await crypto.subtle.sign('HMAC', c, m);
+	return btoa(String.fromCharCode(...new Uint8Array(s)))
+}
+
+async function HMAC(key, message){
+  const g = str => new Uint8Array([...unescape(encodeURIComponent(str))].map(c => c.charCodeAt(0))),
+  k = g(key),
+  m = g(message),
+  c = await crypto.subtle.importKey('raw', k, { name: 'HMAC', hash: 'SHA-256' },true, ['sign']),
+  s = await crypto.subtle.sign('HMAC', c, m);
+  return btoa(String.fromCharCode(...new Uint8Array(s)))
+}
 
 String.prototype.isJsonString = function()
 {
